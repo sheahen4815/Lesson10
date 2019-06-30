142f5c7751c03da246f879d6ac7b80c1e5ff0e05
Donna Sheahen <sheahen4815@charter.net>
2019-06-30 12:06:36 -0500

    Added test for the REST controller

--- src/main/java/edu/dcccd/lesson10/entities/Greeting.java
+++ src/main/java/edu/dcccd/lesson10/entities/Greeting.java
@@ -1,14 +1,21 @@
 package edu.dcccd.lesson10.entities;
 
 
-import lombok.Data;
+import lombok.*;
 
 @Data
 public class Greeting {
 
     private String greeting;
 
+    public Greeting() {
+    }
+
     public Greeting(String greeting) {
         this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
     }
 }
---
+++ src/test/java/edu/dcccd/lesson10/controllers/HelloRestControllerTests.java
@@ -0,0 +1,38 @@
+package edu.dcccd.lesson10.controllers;
+
+import edu.dcccd.lesson10.entities.Greeting;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloRestControllerTests {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void greetWithoutName() {
+        ResponseEntity<Greeting> entity = template.getForEntity("/rest", Greeting.class);
+        assertEquals (HttpStatus.OK, entity.getStatusCode());
+        assertEquals (MediaType.APPLICATION_JSON_UTF8, entity.getHeaders().getContentType());
+        Greeting response = entity.getBody();
+        assertEquals ("Hello, World!", response.getGreeting());
+    }
+
+    @Test
+    public void greetWithName() {
+        Greeting response = template.getForObject("/rest?name=Spring Boot", Greeting.class);
+        assertEquals ("Hello, Spring Boot!", response.getGreeting());
+    }
+}
